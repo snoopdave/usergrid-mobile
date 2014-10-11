9cef9a4a8e676b3d919e1abd6e7fd2e4d4c3325c
Dave Johnson <dmjohnson@apigee.com>
2014-10-11 12:36:14 -0400

    Remove build directories that should have been ignored.

--- checkinv4/platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/ApigeeCorporation/Desktop/Intro To Usergrid/usergrid-mobile/checkinv4/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
--- checkinv4/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/ApigeeCorporation/Desktop/Intro To Usergrid/usergrid-mobile/checkinv4/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/ApigeeCorporation/Desktop/Intro To Usergrid/usergrid-mobile/checkinv4/platforms/android/AndroidManifest.xml \
